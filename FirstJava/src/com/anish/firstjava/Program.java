fa955a1a90a62abc666f7557f4c95d429066b1be
anishrajbhandari <anish.rajbhandari98@gmail.com>
2016-06-22 11:59:26 +0545

    Hello World Added

--- FirstJava/src/com/anish/firstjava/Program.java
+++ FirstJava/src/com/anish/firstjava/Program.java
@@ -17,7 +17,7 @@ public class Program {
     public static void main(String[] args) {
         // TODO code application logic here
         for(int i= 1;i<=10;i++){
-            System.out.println(i);
+            System.out.println(i + "Hello World");
         }
     }
     
